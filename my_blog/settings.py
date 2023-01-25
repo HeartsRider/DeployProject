e4110f7b569b6dc0bfef875bc91a3f7a6949858f
wojiaoyibaqiang <2433887004p@gmail.com>
2023-01-25 11:21:48 +0800

    first try of deployment

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -23,11 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm(&1_@b(^yyvg^6^iuyh#kver2m6&%pkp&0-l1+_2-%a#i6%xt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.lvguoBlog.com','121.43.189.115']
 
-
+# 静态文件收集目录
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 # Application definition
 
 INSTALLED_APPS = [
